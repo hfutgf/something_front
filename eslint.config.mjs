f6ac4bb4d2839e50a9135cb3218cd2d0c900d22c
hfutgf <beretax308@gmail.com>
2025-03-18 16:48:48 +0500

    feat: set up eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,46 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import airbnb from "eslint-config-airbnb";
+import prettier from "eslint-config-prettier";
+import next from "eslint-plugin-next";
+import simpleImportSort from "eslint-plugin-simple-import-sort";
+import tailwindcss from "eslint-plugin-tailwindcss";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const compat = new FlatCompat({
-  baseDirectory: __dirname,
-});
-
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+export default [
+  eslint.configs.recommended,
+  airbnb,
+  prettier,
+  tailwindcss.configs.recommended,
+  next.configs["core-web-vitals"],
+  {
+    plugins: {
+      prettier,
+      tailwindcss,
+      "simple-import-sort": simpleImportSort,
+    },
+    rules: {
+      "react/jsx-filename-extension": [
+        1,
+        { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+      ],
+      "no-console": "warn",
+      "simple-import-sort/imports": "error",
+      "import/no-duplicates": "error",
+      "newline-before-return": "error",
+      "no-unused-vars": ["error", { args: "none" }],
+      "import/no-extraneous-dependencies": "off",
+      "import/extensions": "off",
+      "react/jsx-props-no-spreading": "off",
+      "react/no-array-index-key": "off",
+      "react/require-default-props": [
+        "error",
+        { ignoreFunctionalComponents: true },
+      ],
+      "tailwindcss/migration-from-tailwind-2": "off",
+      "eol-last": ["error", "always"],
+      "react/function-component-definition": "off",
+      "jsx-a11y/no-static-element-interactions": "off",
+      "jsx-a11y/click-events-have-key-events": "off",
+      "jsx-a11y/no-noninteractive-element-interactions": "off",
+    },
+  },
 ];
-
-export default eslintConfig;
